d54e6e6816965d365e86cd3fbf3d4ff339035558
Bipin Gyawali <gyawalibipin133@gmail.com>
2024-12-15 20:41:36 +0000

    requiments.txt file added for deployment

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASEURL = 'postgresql://postgres:bipin123@localhost/FasApiCRUD'
+# DATABASEURL = 'postgresql://postgres:bipin123@localhost/FasApiCRUD'
+
+DATABASEURL = 'postgresql://fast_api_crud_user:QQu12CfPkwueNe8z3KBlcyshunkH3vl4@dpg-ctfjskt6l47c73b9e1kg-a.frankfurt-postgres.render.com/fast_api_crud'
 
 engine = create_engine(DATABASEURL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
